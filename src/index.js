97f3768af90af19553a0dbc5d7ae47ced2441fc9
Edén <claudio.ruiz@bootcamp-upgrade.com>
2021-09-07 15:52:21 +0200

    FEAT: created database connection

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import connect from '../utils/db.config.js';
+
+connect();
---
+++ utils/db.config.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv  from 'dotenv';
+
+dotenv.config();
+const DB_URL = process.env.MONGODB_URL;
+
+const connect = async () => {
+    try {
+        const db = await mongoose.connect(DB_URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        const { name, host } = db.connection; 
+        console.log(`Connected to database ${name} on host ${host}.`);
+    } catch (error) {
+        console.log(`Error connecting to database ${name} on host ${host}.`);
+    }
+};
+
+export default connect;
