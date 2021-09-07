09e4bac0b76acd931bacbf7ced1e06d640603be7
Francisco Javier Galán Guerrero <galan.guererro@bootcamp-upgrade.com>
2021-09-07 17:33:54 +0200

    models done

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,17 +1,20 @@
 import Schema from "mongoose/Schema";
 import mongoose from "mongoose";
 
-const userSchema = new Schema({
-  role: { type: String, default: "user" },
-  name: { type: String, default: "user" },
-  surname: { type: String, default: "user_surname" },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  avatar: { type: String, default: "" },
-  favs: [{ type: String }],
-  location: [{ type: mongoose.types.ObjectId, ref: "Location" }],
-});
+const userSchema = new Schema(
+  {
+    role: { type: String, default: "user" },
+    name: { type: String, default: "user" },
+    surname: { type: String, default: "user_surname" },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: "" },
+    favs: [{ type: String }],
+    location: [{ type: mongoose.types.ObjectId, ref: "Location" }],
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema); 
+const User = mongoose.model("User", userSchema);
 
 export default User;
