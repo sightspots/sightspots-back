e59c293d0e344f3c94bff5fe85f4eae28a5df41d
GonzaDeLarge <calvo.pellicer@bootcamp-upgrade.com>
2021-09-08 17:19:15 +0200

    FEAT increase cookie expire date & change model user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,13 +7,14 @@ const router = express.Router();
 
 // TODO Eliminar endpoint "/users" al terminar testeos
 router.get("/users", userCrtl.getUsers);// * los usuarios
-router.get("/user/:id", userCrtl.getUser);// 1 usuario
 router.delete("/delete/:id", userCrtl.deleteUser);// borrar usuario
 router.put("/edit/:id", multer.multerUpload.single('avatar'), cloudinary.cloudinaryAvatarUpload, userCrtl.putUser);// editar usuario
 router.put("/put-fav/:id", userCrtl.putFav);//añadir a favoritos
 // TODO terminar controlador de eliminar favorito y crear lista
 // router.put("/delete-fav/:id");//eliminar de favoritos
 // router.post("/create-list", userCrtl.postList);//crear lista
+router.get("/:id", userCrtl.getUser);// 1 usuario
+
 
 
 export default router;
