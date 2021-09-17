0eaaeba19744ccfd46a71ba0c0c7a7261d27f613
Edén <claudio.ruiz@bootcamp-upgrade.com>
2021-09-17 19:25:18 +0200

    CHORE: cleaned up TODOs

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,52 +7,48 @@ const registerGet = (req, res, next) => {
 
 const registerPost = async (req, res, next) => {
 
-    const done = ( error, user) =>{
-            if (error) return next(error);
-        
-            req.login(user, (error) => (error ? next(error) : res.json("Usuario registrado con éxito")));
-        
+    const done = (error, user) => {
+        if (error) return next(error);
+
+        req.login(user, (error) => (error ? next(error) : res.json(user)));
     }
 
-    passport.authenticate("register",done)(req);
+    passport.authenticate("register", done)(req);
 }
 
 const loginGet = (req, res, next) => {
 
-    return res.json("Página de loguin");
+    return res.json("Página de login");
 }
 
-const loginPost = (req, res, next) =>{
+const loginPost = (req, res, next) => {
+    const done = (error, user) => {
 
-
-    const done = ( error, user) => {
-
-        if(error){
+        if (error) {
             return next(error);
         }
-        req.logIn(user, (error)=>{
-
+        req.logIn(user, (error) => {
             console.log("Usuario logueado -> ", user);
 
-            if(error){
+            if (error) {
                 return next(error);
             }
             return res.status(200).json(user);
         })
     };
-    
+
     passport.authenticate("login", done)(req);
 }
 
-const logoutPost = (req, res, next) =>{
-   
-    if(req.user){
-    req.logout();
-    req.session.destroy(() =>{
-        res.clearCookie("connect.sid");
-        return res.json("Has salido");
-    });
-}
+const logoutPost = (req, res, next) => {
+
+    if (req.user) {
+        req.logout();
+        req.session.destroy(() => {
+            res.clearCookie("connect.sid");
+            return res.json("Has salido");
+        });
+    }
 }
 
 export default { registerGet, registerPost, loginGet, loginPost, logoutPost };
